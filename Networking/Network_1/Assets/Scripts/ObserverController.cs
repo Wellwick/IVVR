b0d563cfaf8a4089183e4a401f04a8d6dec37583
Michael Boyle <m.j.boyle@warwick.ac.uk>
2017-11-03 13:18:41 +0000

    Added VR Unity test scene for working picking up and teleporting on accepted surfaces

---
+++ Networking/Network_1/Assets/Scripts/ObserverController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class ObserverController : NetworkBehaviour {
+	public Camera cam;
+
+	public GameObject pingObject;
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log("Observer Entered the World");
+		if (!isLocalPlayer) {
+			cam.enabled = false;
+		} else {
+			cam.enabled = true;
+		}
+
+}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!isLocalPlayer) {
+			return;
+		}
+
+		if (Input.GetMouseButtonDown(0)) {
+			Debug.Log("Pressed left click.");
+			CmdSpawnPing();
+			Debug.Log("ping at " + Input.mousePosition);
+		}
+	}
+
+	[Command]
+	void CmdSpawnPing () {
+		Ray ray;
+		RaycastHit hit;
+		ray = cam.ScreenPointToRay(Input.mousePosition);
+		if (Physics.Raycast(ray, out hit, 100.0f)) {
+			Debug.Log(hit.collider.name);
+			if (hit.collider.name == "Plane") {
+
+				var ping = Instantiate(pingObject, hit.point, Quaternion.identity) as GameObject;
+				NetworkServer.Spawn(ping);
+			}
+		}
+	}
+}
+
+
+
+
---
+++ Networking/Network_1/Assets/Scripts/PingBehaviour.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+public class PingBehaviour : NetworkBehaviour {
+	public float timeTillDie;
+
+	private float startTime;
+
+	// Use this for initialization
+	void Start () {
+		startTime = Time.timeSinceLevelLoad;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Time.timeSinceLevelLoad - startTime >= timeTillDie) {
+			Destroy(gameObject);
+		}
+		
+	}
+}
