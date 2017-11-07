fe56b5420436c3d4902501af1e6261b083b42178
Thomas Baxendale <tombaxendale95@gmail.com>
2017-11-07 20:26:02 +0000

    network test using LLAPI

---
+++ Networking/Network_2/Assets/Scripts/Network/NetworkManager.cs
@@ -0,0 +1,117 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+using UnityEngine.Networking;
+public class NetworkManager : MonoBehaviour {
+
+	public static bool serverHosted = false;
+
+	public GameObject buttonHost;
+	public GameObject buttonClient;
+	public GameObject buttonDisconnect;
+
+	private int socketId;
+	private int hostId;
+	private int connectionId;
+
+	// Use this for initialization
+	void Start () {
+		Button btnHost = buttonHost.GetComponent<Button>();
+		Button btnClient = buttonClient.GetComponent<Button>();
+		Button btnDisconnect = buttonDisconnect.GetComponent<Button>();
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (serverHosted) {
+			int recHostId;
+			int connectionId;
+			int channelId;
+			byte[] recBuffer = new byte[1024];
+			int bufferSize = 1024;
+			int dataSize;
+			byte error;
+			NetworkEventType recData = NetworkTransport.Receive(out recHostId, out connectionId, out channelId, recBuffer, bufferSize, out dataSize, out error);
+			switch (recData) {
+				case NetworkEventType.Nothing:
+					break;
+				case NetworkEventType.ConnectEvent:
+					Debug.Log("Connection request from id: " + connectionId + " Received");
+					break;
+				case NetworkEventType.DataEvent:
+					Debug.Log("Data Received");
+					break;
+				case NetworkEventType.DisconnectEvent:
+					Debug.Log("Disconnect Received");
+					break;
+			}
+		}
+
+	}
+
+	public void StartHost() {
+		buttonHost.SetActive(false);
+		buttonClient.SetActive(false);
+		Debug.Log("Cleared UI");
+
+		NetworkTransport.Init();
+		Debug.Log("Host Started");
+
+		ConnectionConfig config = new ConnectionConfig();
+		int myReiliableChannelId = config.AddChannel(QosType.Reliable);
+		int myUnreliableChannelId = config.AddChannel(QosType.Unreliable);
+		HostTopology topology = new HostTopology(config, 1);
+		int socketId = NetworkTransport.AddHost(topology, 7777);
+		Debug.Log(socketId);
+
+		serverHosted = true;
+
+
+
+	}
+
+	public void StartClient() {
+		buttonHost.SetActive(false);
+		buttonClient.SetActive(false);
+		Debug.Log("Cleared UI");
+
+		buttonDisconnect.SetActive(true);
+
+		NetworkTransport.Init();
+		Debug.Log("Host Started");
+
+		ConnectionConfig config = new ConnectionConfig();
+		int myReiliableChannelId = config.AddChannel(QosType.Reliable);
+		int myUnreliableChannelId = config.AddChannel(QosType.Unreliable);
+		HostTopology topology = new HostTopology(config, 1);
+		int socketId = NetworkTransport.AddHost(topology, 55607);
+		Debug.Log(socketId);
+
+		byte error;
+		int connectionId = NetworkTransport.Connect(socketId, "127.0.0.1", 7777, 0, out error);
+		Debug.Log("Connected to server. ConnectionId: " + connectionId);
+		serverHosted = true;
+
+	}
+
+	public void StartDisconnect() {
+		buttonDisconnect.SetActive(false);
+		byte error;
+		NetworkTransport.Disconnect(hostId, connectionId, out error);
+		Debug.Log("Disconnected");
+
+		buttonHost.SetActive(true);
+		buttonClient.SetActive(true);
+	}
+
+	private void SendMessage() {
+
+
+
+	}
+
+
+}
---
+++ Networking/Network_2/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour {
+
+	public GameObject spawnable;
+
+	// Use this for initialization
+	void Start() {
+
+	}
+
+	// Update is called once per frame
+	void Update() {
+		if (NetworkManager.serverHosted) {
+			if (Input.GetMouseButtonDown(0)) {
+				Vector3 objectPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+				objectPos.z = 0f;
+				Instantiate(spawnable, objectPos, Quaternion.identity);
+			}
+		}
+	}
+}
