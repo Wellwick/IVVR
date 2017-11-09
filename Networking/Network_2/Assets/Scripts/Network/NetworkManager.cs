4d78f997c431f3b0dd26b163d4b227f8601c4e07
Thomb95 <tombaxendale95@gmail.com>
2017-11-09 02:07:26 +0000

    small update, working towards sorting out some slight network issues on restart

--- Networking/Network_2/Assets/Scripts/Network/NetworkManager.cs
+++ Networking/Network_2/Assets/Scripts/Network/NetworkManager.cs
@@ -2,177 +2,171 @@
 using System.Runtime.Serialization.Formatters.Binary;
 using UnityEngine;
 using UnityEngine.Networking;
-using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class NetworkManager : MonoBehaviour {
 
-	public static bool serverHosted = false;
-	public static NetworkManager instance;
+    public static bool serverHosted = false;
 
-	public GameObject buttonHost;
-	public GameObject buttonClient;
-	public GameObject buttonDisconnect;
+    public GameObject buttonHost;
+    public GameObject buttonClient;
+    public GameObject buttonDisconnect;
+    public LevelManager levelmanager;
 
-	private int socketId;
-	private int hostId;
-	private int connectionId;
-	private int myReliableChannelId;
-	private int myUnreliableChannelId;
-	private SpawnManager spawnmanager;
-	private bool networkInitialised;
+    private int socketId;
+    private int hostId;
+    private int connectionId;
+    private int myReliableChannelId;
+    private int myUnreliableChannelId;
+    private SpawnManager spawnmanager;
+    private bool networkInitialised;
 
-	// Use this for initialization
-	void Start() {
-		Button btnHost = buttonHost.GetComponent<Button>();
-		Button btnClient = buttonClient.GetComponent<Button>();
-		Button btnDisconnect = buttonDisconnect.GetComponent<Button>();
-		spawnmanager = GameObject.FindObjectOfType<SpawnManager>();
-		networkInitialised = false;
-		if (instance == null) {
-			DontDestroyOnLoad(this);
-			instance = this;
-		} else {
-			Destroy(this);
-		}
-		
-	}
+    // Use this for initialization
+    void Start() {
+        Button btnHost = buttonHost.GetComponent<Button>();
+        Button btnClient = buttonClient.GetComponent<Button>();
+        Button btnDisconnect = buttonDisconnect.GetComponent<Button>();
+        spawnmanager = GameObject.FindObjectOfType<SpawnManager>();
+        networkInitialised = false;
 
-	// Update is called once per frame
-	void Update() {
-		if (serverHosted) {
-			int recHostId;
-			int connectionId;
-			int channelId;
-			byte[] recBuffer = new byte[1024];
-			int bufferSize = 1024;
-			int dataSize;
-			byte error;
-			NetworkEventType recData = NetworkTransport.Receive(out recHostId, out connectionId, out channelId, recBuffer, bufferSize, out dataSize, out error);
-			switch (recData) {
-				case NetworkEventType.Nothing:
-					break;
-				case NetworkEventType.ConnectEvent:
-					Debug.Log("Connection request from id: " + connectionId + " Received");
-					this.connectionId = connectionId;
-					break;
-				case NetworkEventType.DataEvent:
-					SerializeableTransform data;
-					Debug.Log("Data Received");
-					BinaryFormatter bf = new BinaryFormatter();
-					using(MemoryStream ms = new MemoryStream(recBuffer)) {
-						data = bf.Deserialize(ms) as SerializeableTransform;
-					}
-					spawnmanager.SpawnObjectNetwork(data);
+    }
 
-					break;
-				case NetworkEventType.DisconnectEvent:
-					serverHosted = false;
-					Debug.Log("Disconnect Received");
-					SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    // Update is called once per frame
+    void Update() {
+        if (serverHosted) {
+            int recHostId;
+            int connectionId;
+            int channelId;
+            byte[] recBuffer = new byte[1024];
+            int bufferSize = 1024;
+            int dataSize;
+            byte error;
+            NetworkEventType recData = NetworkTransport.Receive(out recHostId, out connectionId, out channelId, recBuffer, bufferSize, out dataSize, out error);
+            switch (recData) {
+                case NetworkEventType.Nothing:
+                    break;
+                case NetworkEventType.ConnectEvent:
+                    Debug.Log("Connection request from id: " + connectionId + " Received");
+                    this.connectionId = connectionId;
+                    break;
+                case NetworkEventType.DataEvent:
+                    SerializeableTransform data;
+                    Debug.Log("Data Received");
+                    BinaryFormatter bf = new BinaryFormatter();
+                    using (MemoryStream ms = new MemoryStream(recBuffer)) {
+                        data = bf.Deserialize(ms) as SerializeableTransform;
+                    }
+                    spawnmanager.SpawnObjectNetwork(data);
 
-					break;
-			}
-		}
+                    break;
+                case NetworkEventType.DisconnectEvent:
+                    serverHosted = false;
+                    Debug.Log("Disconnect Received");
+                    levelmanager.LoadLevel(SceneManager.GetActiveScene().name);
 
-	}
+                    break;
+            }
+        }
 
-	public void StartHost() {
-		buttonHost.SetActive(false);
-		buttonClient.SetActive(false);
-		Debug.Log("Cleared UI");
-		if (!networkInitialised) {
-			NetworkTransport.Init();
-			Debug.Log("Host Started");
-			ConnectionConfig config = new ConnectionConfig();
-			int myReiliableChannelId = config.AddChannel(QosType.Reliable);
-			int myUnreliableChannelId = config.AddChannel(QosType.Unreliable);
-			HostTopology topology = new HostTopology(config, 1);
-			int socketId = NetworkTransport.AddHost(topology, 7777);
-			Debug.Log(socketId);
-			networkInitialised = true;
-		}
+    }
 
-		serverHosted = true;
+    public void StartHost() {
+        buttonHost.SetActive(false);
+        buttonClient.SetActive(false);
+        Debug.Log("Cleared UI");
+        if (!networkInitialised) {
+            NetworkTransport.Init();
+            Debug.Log("Host Started");
+            ConnectionConfig config = new ConnectionConfig();
+            int myReiliableChannelId = config.AddChannel(QosType.Reliable);
+            int myUnreliableChannelId = config.AddChannel(QosType.Unreliable);
+            HostTopology topology = new HostTopology(config, 1);
+            int socketId = NetworkTransport.AddHost(topology, 7777);
+            Debug.Log(socketId);
+            networkInitialised = true;
+        }
 
-	}
+        serverHosted = true;
 
-	public void StartClient() {
-		buttonHost.SetActive(false);
-		buttonClient.SetActive(false);
-		Debug.Log("Cleared UI");
-		buttonDisconnect.SetActive(true);
+    }
 
-		if (!networkInitialised) {
-			NetworkTransport.Init();
-			Debug.Log("Host Started");
+    public void StartClient() {
+        buttonHost.SetActive(false);
+        buttonClient.SetActive(false);
+        Debug.Log("Cleared UI");
+        buttonDisconnect.SetActive(true);
 
-			ConnectionConfig config = new ConnectionConfig();
-			myReliableChannelId = config.AddChannel(QosType.Reliable);
-			myUnreliableChannelId = config.AddChannel(QosType.Unreliable);
-			HostTopology topology = new HostTopology(config, 1);
-			int socketId = NetworkTransport.AddHost(topology, 55607);
-			Debug.Log(socketId);
-			networkInitialised = true;
-		}
-		byte error;
-		int connectionId = NetworkTransport.Connect(socketId, "127.0.0.1", 7777, 0, out error);
-		Debug.Log("Connected to server. ConnectionId: " + connectionId);
-		serverHosted = true;
+        if (!networkInitialised) {
+            NetworkTransport.Init();
+            Debug.Log("Host Started");
 
-	}
+            ConnectionConfig config = new ConnectionConfig();
+            myReliableChannelId = config.AddChannel(QosType.Reliable);
+            myUnreliableChannelId = config.AddChannel(QosType.Unreliable);
+            HostTopology topology = new HostTopology(config, 1);
+            int socketId = NetworkTransport.AddHost(topology, 55607);
+            Debug.Log(socketId);
+            networkInitialised = true;
+        }
+        byte error;
+        int connectionId = NetworkTransport.Connect(socketId, "127.0.0.1", 7777, 0, out error);
+        Debug.Log("Connected to server. ConnectionId: " + connectionId);
+        serverHosted = true;
 
-	public void StartDisconnect() {
-		serverHosted = false;
-		byte error;
-		NetworkTransport.Disconnect(hostId, connectionId, out error);
-		Debug.Log("Disconnected");
-		SceneManager.LoadScene(SceneManager.GetActiveScene().name);
-	}
+    }
 
-	public void SendSerializeableTransform(GameObject spawnable) {
-		Debug.Log("sending message on network");
-		SerializeableTransform st = new SerializeableTransform(spawnable.transform);
-		Debug.Log(st);
-		BinaryFormatter bf = new BinaryFormatter();
-		using (MemoryStream ms = new MemoryStream()) {
-			bf.Serialize(ms, st);
-			byte error;
-			Debug.Log(ms.ToArray());
-			Debug.Log(ms.ToArray().Length);
-			NetworkTransport.Send(hostId, connectionId, myReliableChannelId, ms.ToArray(), 1024, out error);
+    public void StartDisconnect() {
+        serverHosted = false;
+        byte error;
+        NetworkTransport.Disconnect(hostId, connectionId, out error);
+        Debug.Log("Disconnected");
+        levelmanager.LoadLevel(SceneManager.GetActiveScene().name);
+    }
+
+    public void SendSerializeableTransform(GameObject spawnable) {
+        Debug.Log("sending message on network");
+        SerializeableTransform st = new SerializeableTransform(spawnable.transform);
+        Debug.Log(st);
+        BinaryFormatter bf = new BinaryFormatter();
+        using (MemoryStream ms = new MemoryStream()) {
+            bf.Serialize(ms, st);
+            byte error;
+            Debug.Log(ms.ToArray());
+            Debug.Log(ms.ToArray().Length);
+            NetworkTransport.Send(hostId, connectionId, myReliableChannelId, ms.ToArray(), 1024, out error);
 
 
-		}
+        }
 
 
-	}
+    }
 
-	[System.Serializable]
-	public class SerializeableTransform {
-		public float posX;
-		public float posY;
-		public float posZ;
-		public float rotX;
-		public float rotY;
-		public float rotZ;
-		public float rotW;
+    [System.Serializable]
+    public class SerializeableTransform {
+        public float posX;
+        public float posY;
+        public float posZ;
+        public float rotX;
+        public float rotY;
+        public float rotZ;
+        public float rotW;
 
 
-		public SerializeableTransform(Transform transform) {
-			posX = transform.position.x;
-			posY = transform.position.y;
-			posZ = transform.position.z;
-			rotX = transform.rotation.x;
-			rotY = transform.rotation.y;
-			rotZ = transform.rotation.z;
-		}
+        public SerializeableTransform(Transform transform) {
+            posX = transform.position.x;
+            posY = transform.position.y;
+            posZ = transform.position.z;
+            rotX = transform.rotation.x;
+            rotY = transform.rotation.y;
+            rotZ = transform.rotation.z;
+        }
 
-		public override string ToString() {
-			return "x = " + posX + "\ny = " + posY + "\nz = " + posZ;
-		}
+        public override string ToString() {
+            return "x = " + posX + "\ny = " + posY + "\nz = " + posZ;
+        }
 
-	}
+    }
 
 
 }
--- Networking/Network_2/Assets/Scripts/SpawnManager.cs
+++ Networking/Network_2/Assets/Scripts/SpawnManager.cs
@@ -1,46 +1,37 @@
 ﻿using UnityEngine;
 
 public class SpawnManager : MonoBehaviour {
+    public GameObject spawnable;
+    public NetworkManager networkmanager;
+    // Use this for initialization
+    void Start() {
+        networkmanager = GameObject.FindObjectOfType<NetworkManager>();
 
-	public static SpawnManager instance;
+    }
 
-	public GameObject spawnable;
-	public NetworkManager networkmanager;
-	// Use this for initialization
-	void Start() {
-		networkmanager = GameObject.FindObjectOfType<NetworkManager>();
-		if (instance == null) {
-			DontDestroyOnLoad(this);
-			instance = this;
-		} else {
-			Destroy(this);
-		}
-
-	}
-
-	// Update is called once per frame
-	void Update() {
-		if (NetworkManager.serverHosted) {
-			if (Input.GetMouseButtonDown(0)) {
-				Vector3 objectPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-				objectPos.z = 0f;
-				GameObject cube = Instantiate(spawnable, objectPos, Quaternion.identity) as GameObject;
-				Debug.Log("Cube position = " + cube.transform.position);
-				if (NetworkManager.serverHosted) {
-					networkmanager.SendSerializeableTransform(cube);
-				}
-			}
-		}
-	}
+    // Update is called once per frame
+    void Update() {
+        if (NetworkManager.serverHosted) {
+            if (Input.GetMouseButtonDown(0)) {
+                Vector3 objectPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                objectPos.z = 0f;
+                GameObject cube = Instantiate(spawnable, objectPos, Quaternion.identity) as GameObject;
+                Debug.Log("Cube position = " + cube.transform.position);
+                if (NetworkManager.serverHosted) {
+                    networkmanager.SendSerializeableTransform(cube);
+                }
+            }
+        }
+    }
 
 
-	public void SpawnObjectNetwork(NetworkManager.SerializeableTransform st) {
-		Debug.Log("spawning object from network");
-		Debug.Log(st);
-		Vector3 pos = new Vector3(st.posX, st.posY, st.posZ);
-		Instantiate(spawnable, pos, Quaternion.identity);
+    public void SpawnObjectNetwork(NetworkManager.SerializeableTransform st) {
+        Debug.Log("spawning object from network");
+        Debug.Log(st);
+        Vector3 pos = new Vector3(st.posX, st.posY, st.posZ);
+        Instantiate(spawnable, pos, Quaternion.identity);
 
-	}
+    }
 }
 
 
