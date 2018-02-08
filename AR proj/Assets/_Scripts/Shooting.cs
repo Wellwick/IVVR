a3b864728d6d4ba5421fb5caad6f4e69d48d1732
Chris <krzysiojanek@gmail.com>
2018-02-08 22:29:10 +0000

    Added beam_damage prefab. ToDo: add methods for enemies being hit, test with tracker

--- AR proj/Assets/_Scripts/Shooting.cs
+++ AR proj/Assets/_Scripts/Shooting.cs
@@ -4,17 +4,18 @@ using UnityEngine;
 
 public class Shooting : MonoBehaviour {
 
+	public GameObject beamPrefab;
+	private GameObject beam;
 
-
-	private UnityARCameraManager ARManager;
 	private Camera m_camera;
 	private Vector3 rayOffset;
 
+
+
 	// Use this for initialization
 	void Start () {
-		ARManager = GameObject.FindObjectOfType<UnityARCameraManager>();
+		rayOffset = new Vector3 (0.0f, -0.1f, 0.0f);
 		m_camera = Camera.main;
-		rayOffset = new Vector3 (0.0f, -0.01f, 0.0f);
 	}
 	
 	// Update is called once per frame
@@ -23,14 +24,14 @@ public class Shooting : MonoBehaviour {
 	}
 
 
-	public GameObject raycast() {
+	public GameObject raycast(Transform parent) {
 
 
 		RaycastHit hit; //Store result of raycast here
 		GameObject result = null; //The GAmeObject hit by the ray
-		Vector3 origin = m_camera.transform.position + rayOffset; //The origin of the ray
-		Vector3 direction = m_camera.transform.forward;//direction of ray
 
+		Vector3 origin = parent.position;
+		Vector3 direction = parent.forward;
 		//Vector3 origin = ARManager.getUnityCameraPosition(); 
 		//Vector3 direction = ARManager.getUnityCameraRotation(); 
 
@@ -45,12 +46,40 @@ public class Shooting : MonoBehaviour {
 
 	public void damage() {
 
+		Transform t = m_camera.transform;
 
-		raycast ();
+		Vector3 origin = t.position + rayOffset; //Offset the ray so that it does not come out from the camera, but rather from below it
+
+		Vector3 direction = t.forward;
+
+
+		GameObject collision;
+
+		Debug.Log ("Shooting damage from " );
+		if ((collision = raycast (t)) != null) {
+			Debug.Log (t.position.x + " " + t.position.y + " " + t.position.z);
+			Vector3 end = collision.transform.position;
+
+			float length = Vector3.Distance (end, origin);
+
+			//lock beam onto enemy origin
+			beam = Instantiate (beamPrefab, origin + 0.5f * (end - origin), Quaternion.Euler(end - origin));
+
+			//beam = Instantiate (beamPrefab, origin, direction);
+
+			beam.transform.localScale = new Vector3 (0.01f, 0.01f, length);
+
+		}
 	}
 
 	public void heal() {
+		
+		Transform t = m_camera.transform;
+		t.position = t.position + rayOffset; //Offset the ray so that it does not come out from the camera, but rather from below it
 
-		raycast ();
+		Instantiate (beamPrefab,t);
+		Debug.Log ("Shooting healing...");
+		raycast (t);
+
 	}
 }
---
+++ AR proj/Assets/_Scripts/beam_damage.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class beam_damage : MonoBehaviour {
+
+	float duration = 5.0f;
+
+	GameObject target;
+	//GameObject source;
+
+
+
+	// Use this for initialization
+	void Start () {
+
+
+	}
+
+
+
+
+	// Update is called once per frame
+	void Update () {
+		
+
+		duration = duration - Time.deltaTime;
+
+		if (duration <= 0) {
+			Destroy (this);
+		}
+	}
+
+
+	public void setTarget(GameObject target) {
+		this.target = target;
+
+
+
+	}
+}
