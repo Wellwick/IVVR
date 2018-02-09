3191707de960e990be440ec32ca6702a0e427f4a
Chris <krzysiojanek@gmail.com>
2018-02-09 00:55:02 +0000

    Functioning beams for shooting and healing. Raycast is done on press, beam exists for 1 second. Beams lock onto center of enemy if hit is successful. ToDo: improve graphics, add particles, make shooting a toggleable/hold-down function with raycasting done on update() rather than on press.

---
+++ AR proj/Assets/_Scripts/Beam.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Beam : MonoBehaviour {
+
+	public GameObject beam;
+
+	private float duration;
+	private GameObject target;
+	private GameObject source;
+	private Vector3 end;
+	private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+
+		target = null;
+		source = null;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+		duration = duration - Time.deltaTime;
+		if (duration <= 0.0f) {
+			beam.SetActive (false);
+		} else {
+
+			Vector3 end;
+			Vector3 origin = source.transform.position + offset;
+
+			if (target != null) {
+				end = target.transform.position;
+			} else {
+				end = origin + (source.transform.forward * 10.0f);
+			}
+
+			float length = Vector3.Distance (end, origin);
+			beam.transform.position = Vector3.Lerp (origin, end, 0.5f);
+			beam.transform.localScale = new Vector3 (0.01f, 0.01f, length);
+			beam.transform.LookAt (end);
+		}
+	}
+
+	public void Shoot(GameObject source, GameObject target, Vector3 offset) {
+
+		this.source = source;
+		this.target = target;
+		this.offset = offset;
+
+		duration = 1.0f;
+		beam.SetActive (true);
+
+	}
+}
--- AR proj/Assets/_Scripts/Shooting.cs
+++ AR proj/Assets/_Scripts/Shooting.cs
@@ -4,8 +4,9 @@ using UnityEngine;
 
 public class Shooting : MonoBehaviour {
 
-	public GameObject beamPrefab;
-	private GameObject beam;
+	//public GameObject beamPrefab;
+	public GameObject beamDamage;
+	public GameObject beamHealing;
 
 	private Camera m_camera;
 	private Vector3 rayOffset;
@@ -16,6 +17,9 @@ public class Shooting : MonoBehaviour {
 	void Start () {
 		rayOffset = new Vector3 (0.0f, -0.1f, 0.0f);
 		m_camera = Camera.main;
+
+		//beamDamage.setSource (m_camera);
+		//beamDamage.setOffset (rayOffset);
 	}
 	
 	// Update is called once per frame
@@ -48,38 +52,33 @@ public class Shooting : MonoBehaviour {
 
 		Transform t = m_camera.transform;
 
-		Vector3 origin = t.position + rayOffset; //Offset the ray so that it does not come out from the camera, but rather from below it
+		GameObject target = null;
 
-		Vector3 direction = t.forward;
+		target = raycast (t);
+
+		if ((target = raycast (t)) != null) {
+			Handheld.Vibrate ();
+			//ENEMY HIT???
+		} 
 
 
-		GameObject collision;
-
-		Debug.Log ("Shooting damage from " );
-		if ((collision = raycast (t)) != null) {
-			Debug.Log (t.position.x + " " + t.position.y + " " + t.position.z);
-			Vector3 end = collision.transform.position;
-
-			float length = Vector3.Distance (end, origin);
-
-			//lock beam onto enemy origin
-			beam = Instantiate (beamPrefab, origin + 0.5f * (end - origin), Quaternion.Euler(end - origin));
-
-			//beam = Instantiate (beamPrefab, origin, direction);
-
-			beam.transform.localScale = new Vector3 (0.01f, 0.01f, length);
-
-		}
+		beamDamage.GetComponent<Beam>().Shoot(m_camera.gameObject, target, rayOffset);
 	}
 
 	public void heal() {
 		
 		Transform t = m_camera.transform;
-		t.position = t.position + rayOffset; //Offset the ray so that it does not come out from the camera, but rather from below it
 
-		Instantiate (beamPrefab,t);
-		Debug.Log ("Shooting healing...");
-		raycast (t);
+		GameObject target = null;
+	
+		target = raycast (t);
+
+		if ((target = raycast (t)) != null) {
+			Handheld.Vibrate ();
+			//ENEMY HIT???
+		} 
+
+		beamHealing.GetComponent<Beam>().Shoot(m_camera.gameObject, target, rayOffset);
 
 	}
 }
