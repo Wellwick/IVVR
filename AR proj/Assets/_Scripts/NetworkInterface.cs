00a1b20911804c9d36e0274c6a47123145a12702
Chris <krzysiojanek@gmail.com>
2018-02-11 21:23:43 +0000

    Added network interface scripts to VR and AR

---
+++ AR proj/Assets/_Scripts/NetworkInterface.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NetworkInterface : MonoBehaviour {
+
+	private UnityARCameraManager ARCameraManager;
+
+	void Start() {
+		ARCameraManager = GameObject.FindObjectOfType<UnityARCameraManager>();
+	}
+
+	void Update() {
+
+	}
+
+	 
+	public static void UpdateTrackerPose(Vector3 pos, Quaternion rot) {
+
+		ARCameraManager.updateTrackerPosition (pos);
+		ARCameraManager.updateTrackerRotation (rot);
+
+	}
+}
