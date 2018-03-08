922eda83242b5c25ab2a2211ed89d35ddfc0f4cc
Chris <krzysiojanek@gmail.com>
2018-03-08 22:49:18 +0000

    Updated beams such that enemies are damaged on a per-particle-collision basis. Enemies must reside in the Enemy layer, the VR player must reside in the Player layer. Beam are now simpler to control, and are children of the camera, with offsets.

--- AR proj/Assets/Scripts/Beam.cs
+++ AR proj/Assets/Scripts/Beam.cs
@@ -14,66 +14,30 @@ public class Beam : MonoBehaviour {
 	}
 
 	public static beamType type;
-	public int damageVal = 0;
+	public float damageVal;
+	public float rotationSpeed;
 	private float duration;
-	private GameObject target;
-	private GameObject source;
-	private Vector3 end;
-	private Vector3 offset;
+
 
 	// Use this for initialization
 	void Start () {
 
-		target = null;
-		source = null;
 	}
 
 	// Update is called once per frame
 	void Update () {
 
-
 		duration = duration - Time.deltaTime;
-		if (duration <= 0.0f || source == null) {
-			//beam.SetActive (false);
+
+		if (duration <= 0.0f) {
 			beam.GetComponent<ParticleSystem>().Stop();
 		} else {
-
-			Vector3 end;
-			Vector3 origin = source.transform.position + offset;
-
-			if (target != null) {
-				end = target.transform.position;
-				switch (type) {
-				case beamType.Damage:
-					EnemyHealth eh = target.transform.parent.GetComponent<EnemyHealth>();
-					if (eh != null) {
-						Debug.Log("damaging by " + damageVal);
-						eh.Damage(damageVal);
-					} else {
-						Debug.LogError("Couldn't find enemy" + target);
-					}
-					break;
-				case beamType.Heal:
-					PlayerHealth ph = target.GetComponent<PlayerHealth>();
-					break;
-				}
-			} else {
-				end = origin + (source.transform.forward * 10.0f);
-			}
-
-			float length = Vector3.Distance (end, origin);
-			beam.transform.position = origin;
-			//beam.transform.position = Vector3.Lerp (origin, end, 0.5f);
-			//beam.transform.localScale = new Vector3 (0.01f, 0.01f, length);
-			beam.transform.LookAt (end);
+			
+			beam.transform.Rotate(0, 0, rotationSpeed * duration * 2);
 		}
 	}
 
-	public void Shoot(GameObject source, GameObject target, Vector3 offset) {
-
-		this.source = source;
-		this.target = target;
-		this.offset = offset;
+	public void Shoot() {
 
 		duration = 1.0f;
 		beam.GetComponent<ParticleSystem>().Play();
---
+++ AR proj/Assets/Scripts/Collision.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collision : MonoBehaviour {
+
+	public ParticleSystem particle;
+    public List<ParticleCollisionEvent> collisionEvents;
+
+	void Start() {
+        particle = GetComponent<ParticleSystem>();
+        collisionEvents = new List<ParticleCollisionEvent>();
+	}	
+
+	void OnTriggerEnter(Collider other) {
+		Debug.Log("onTriggerEnter");
+        //Destroy(other.gameObject);
+    }
+
+    void OnParticleCollision(GameObject other)
+    {
+        int numCollisionEvents = particle.GetCollisionEvents(other, collisionEvents);
+		Debug.Log("onparticlecollision");
+        Rigidbody rb = other.GetComponent<Rigidbody>();
+        int i = 0;
+
+        while (i < numCollisionEvents)
+        {
+            /*if (rb)
+            {
+                Vector3 pos = collisionEvents[i].intersection;
+                Vector3 force = collisionEvents[i].velocity * 10;
+                rb.AddForce(force);
+            }*/
+            i++;
+        }
+        if (i > 0) {
+            Debug.Log("Damaging enemy by " + i);
+            //Damage(i);
+        }
+    }
+
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
