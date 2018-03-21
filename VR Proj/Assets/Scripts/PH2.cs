9e1b06d62bfe8536cc7f858e2bdc0cb7f7e83969
Thomas Stanley <tomstanley52@gmail.com>
2018-03-21 18:09:45 +0000

    added healthbar assets to vr

---
+++ VR Proj/Assets/Scripts/PH2.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+[System.Serializable]
+public class PH2 : MonoBehaviour
+{
+    // Use this for initialization
+    public int maxHealth = 100;
+    public int currentHealth;
+    public Slider healthSlider;
+
+    public GameObject healthIndicator;
+    public GameObject healthIndicatorinv;
+    private Material indicatorMaterial;
+    private Material indicatorMaterialinv;
+
+
+    public GameObject Indicator;
+    public Camera cam;
+
+
+    private void Start()
+    {
+        SetHealth(maxHealth);
+        // healthSlider.maxValue = maxHealth;
+        indicatorMaterial = healthIndicator.GetComponent<MeshRenderer>().material;
+        indicatorMaterialinv = healthIndicatorinv.GetComponent<MeshRenderer>().material;
+    }
+
+    private void Awake()
+    {
+        indicatorMaterial = healthIndicator.GetComponent<MeshRenderer>().material;
+        indicatorMaterialinv = healthIndicatorinv.GetComponent<MeshRenderer>().material;
+    }
+
+    public int GetHealth()
+    {
+        return currentHealth;
+    }
+
+    public void SetHealth(int health)
+    {
+        currentHealth = health;
+        // healthSlider.value = currentHealth;
+        indicatorMaterial.SetFloat("_cutoff", 1f - ((float)currentHealth / (float)maxHealth));
+        indicatorMaterialinv.SetFloat("_cutoff", ((float)currentHealth / (float)maxHealth));
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        //healthSlider.value = currentHealth;
+
+        //indicatorMaterial = healthIndicator.GetComponent<MeshRenderer>().material;
+        indicatorMaterial.SetFloat("_Cutoff", 1f - (float)currentHealth / (float)maxHealth);
+        indicatorMaterialinv.SetFloat("_Cutoff", (float)currentHealth / (float)maxHealth);
+        Indicator.transform.LookAt(cam.transform);
+    }
+
+    void Death()
+    {
+
+    }
+}
--- VR Proj/Assets/Scripts/PlayerHealth.cs
+++ VR Proj/Assets/Scripts/PlayerHealth.cs
@@ -3,17 +3,48 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 [System.Serializable]
-public class PlayerHealth : MonoBehaviour {
+public class PlayerHealth : MonoBehaviour
+{
     // Use this for initialization
     public int maxHealth = 100;
     public int currentHealth;
-    public Slider healthSlider;
 
+    //green part
+    public GameObject healthIndicator;
+    //red part
+    public GameObject healthIndicatorinv;
+
+    //the materials
+    private Material indicatorMaterial;
+    private Material indicatorMaterialinv;
+
+    //a handle to get at tracker position
+    public GameObject trackObject;
+
+    //gameobject for bars to be childed to - allows manipulations in normal space, so bars are horizontal
+    public GameObject Indicator;
+
+    //camera to point at
+    public Camera cam;
+
+
+    private void Start()
+    {
+        //get private variables - instantiated materials and script
+        indicatorMaterial = healthIndicator.GetComponent<MeshRenderer>().material;
+        indicatorMaterialinv = healthIndicatorinv.GetComponent<MeshRenderer>().material;
+
+        //setup health
+        SetHealth(maxHealth);
+    }
 
     private void Awake()
     {
-        SetHealth(maxHealth);
-        healthSlider.maxValue = maxHealth;
+
+        //indicatorMaterial = healthIndicator.GetComponent<MeshRenderer>().material;
+        //indicatorMaterialinv = healthIndicatorinv.GetComponent<MeshRenderer>().material;
+        //UnityARCameraManager = ARcameraManager.GetComponent<UnityARCameraManager>();
+        //SetHealth(maxHealth);
     }
 
     public int GetHealth()
@@ -23,17 +54,24 @@ public class PlayerHealth : MonoBehaviour {
 
     public void SetHealth(int health)
     {
+        //main way to change health.
         currentHealth = health;
-        healthSlider.value = currentHealth;
+        indicatorMaterial.SetFloat("_cutoff", 1f - ((float)currentHealth / (float)maxHealth));
+        indicatorMaterialinv.SetFloat("_cutoff", ((float)currentHealth / (float)maxHealth));
     }
 
-    void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        healthSlider.value = currentHealth;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //set transform to be towards cameras
+        //Indicator.transform.position = UnityARCameraManager.tracker_position;
+
+        //TESTING ONLY -- updates when changed in editor
+        indicatorMaterial.SetFloat("_Cutoff", 1f - (float)currentHealth / (float)maxHealth);
+        indicatorMaterialinv.SetFloat("_Cutoff", (float)currentHealth / (float)maxHealth);
+        Indicator.transform.LookAt(cam.transform);
+        gameObject.transform.transform.position = trackObject.transform.position;
     }
 
     void Death()
