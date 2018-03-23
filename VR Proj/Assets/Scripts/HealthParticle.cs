d0c55efde6bb83aa813c2cd08b5d7c6f354735e2
Michael Boyle <m.j.boyle@warwick.ac.uk>
2018-03-23 03:28:52 +0000

    Made HealthChunk sprites move to enemy that caused the damage when they appear (using linear interpolation) and destroy upon reaching the enemy

---
+++ VR Proj/Assets/Scripts/HealthParticle.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthParticle : MonoBehaviour {
+
+	public Transform camera;
+	public Transform target;
+
+	// Made use of Unity's Lerp page
+	private float startTime;
+	private Vector3 startPos;
+	public float timeToTarget = 2.0f;
+
+
+
+	// Use this for initialization
+	void Start () {
+		startTime = Time.time;	
+		startPos = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float timeToDate = Time.time - startTime;
+		if (timeToDate >= timeToTarget) { 
+			Destroy(gameObject);
+			return;
+		}
+		transform.LookAt(camera.position, -Vector3.up);
+		// Need to linearly interpolate to the moving enemy
+		transform.position = Vector3.Lerp(startPos, target.position, timeToDate/timeToTarget);
+	}
+}
