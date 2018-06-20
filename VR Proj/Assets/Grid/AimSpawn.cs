02c192327eb6b753aaecd6e5d0dc3c62aeeb9693
Wellwick <mbbexley@hotmail.co.uk>
2018-06-20 15:05:35 +0100

    Added a building scene, currently VR only

---
+++ VR Proj/Assets/Grid/AimSpawn.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AimSpawn : MonoBehaviour {
+    
+    private SteamVR_TrackedController trackedObj;
+    public GameObject aimPrefab;
+    public Grid grid;
+    private GameObject aim;
+    private Transform aimTransform;
+
+    public LayerMask spawnerMask;
+    private GameObject spawning;
+    private Cube spawnFrom;
+
+    // Use this for initialization
+    void Start () {
+        aim = Instantiate(aimPrefab);
+        aimTransform = aim.transform;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        try
+        {
+            if (Controller.GetPress(SteamVR_Controller.ButtonMask.Touchpad))
+            {
+                RaycastHit hit;
+
+
+                if (Physics.Raycast(trackedObj.transform.position, transform.forward, out hit, 100,
+                        spawnerMask))
+                {
+                    ShowAim(hit);
+                    if (spawning != null)
+                        spawning.GetComponent<MeshRenderer>().enabled = false;
+                    spawning = hit.transform.gameObject;
+                    spawning.GetComponent<MeshRenderer>().enabled = true;
+                    spawnFrom = hit.transform.GetComponentInParent<Cube>();
+                }
+            }
+            else
+            {
+                aim.SetActive(false);
+            }
+
+            //Only allow pulling objects when game is active
+            if (Controller.GetHairTriggerDown() && spawning != null)
+            {
+                // TODO spawn the object
+                // choose which of the sides it is
+                if (spawning == spawnFrom.right)            grid.AddCube(spawnFrom.GetX() + 1, spawnFrom.GetY(), spawnFrom.GetZ());
+                else if (spawning == spawnFrom.left)       grid.AddCube(spawnFrom.GetX() - 1, spawnFrom.GetY(), spawnFrom.GetZ());
+                else if (spawning == spawnFrom.front)       grid.AddCube(spawnFrom.GetX(), spawnFrom.GetY(), spawnFrom.GetZ() + 1);
+                else if (spawning == spawnFrom.back)        grid.AddCube(spawnFrom.GetX(), spawnFrom.GetY(), spawnFrom.GetZ() - 1);
+                else if (spawning == spawnFrom.above)       grid.AddCube(spawnFrom.GetX(), spawnFrom.GetY() + 1, spawnFrom.GetZ());
+                else if (spawning == spawnFrom.below)        grid.AddCube(spawnFrom.GetX(), spawnFrom.GetY() - 1, spawnFrom.GetZ());
+            }
+        }
+        catch (MissingReferenceException e)
+        {
+
+        }
+    }
+
+
+
+    private SteamVR_Controller.Device Controller
+    {
+        get { return SteamVR_Controller.Input((int)trackedObj.controllerIndex); }
+    }
+
+    void Awake()
+    {
+        trackedObj = GetComponent<SteamVR_TrackedController>();
+    }
+
+    private void ShowAim(RaycastHit hit)
+    {
+        aim.SetActive(true);
+
+        aimTransform.position = Vector3.Lerp(trackedObj.transform.position, hit.point, .5f);
+        aimTransform.LookAt(hit.point);
+        aimTransform.localScale = new Vector3(aimTransform.localScale.x, aimTransform.localScale.y,
+            hit.distance);
+
+    }
+
+}
---
+++ VR Proj/Assets/Grid/Grid.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grid : MonoBehaviour {
+
+    public int width, height, depth;
+    public GameObject cube;
+    private GameObject[,,] cubes;
+    
+
+	// Use this for initialization
+	void Start () {
+        cubes = new GameObject[width, height, depth];
+        for (int x = 0; x < width; x++) {
+            for (int z = 0; z < depth; z++)
+            {
+                AddCube(x, 0, z);
+            }
+        }
+
+	}
+	
+    public void AddCube(int x, int y, int z)
+    {
+        if (x < width && x >= 0 && y < height && y >= -1 && z < depth && z >= 0)
+        {
+            GameObject cell = GameObject.Instantiate(cube, new Vector3(transform.position.x + (0.2f * x), transform.position.y + (0.2f * y), transform.position.z + (0.2f * z)), new Quaternion());
+            if (y >= 0)
+                cubes[x, y, z] = cell;
+
+            bool above = y < height - 1 && cubes[x, y + 1, z] == null;
+            bool below = y > 0          && cubes[x, y - 1, z] == null;
+            bool front = z < depth - 1  && cubes[x, y, z + 1] == null;
+            bool back = z > 0           && cubes[x, y, z - 1] == null;
+            bool right = x < width - 1  && cubes[x + 1, y, z] == null;
+            bool left = x > 0           && cubes[x - 1, y, z] == null;
+
+            cell.GetComponent<Cube>().SetCoord(x, y, z, above, below, front, back, right, left);
+
+            // Need to go over surrounding cells and remove relevant colliders
+            if (y < height - 1 && cubes[x, y + 1, z] != null)
+            {
+                cubes[x, y + 1, z].GetComponent<Cube>().below.SetActive(false);
+            }
+
+            if (y > 0 && cubes[x, y - 1, z] != null)
+            {
+                cubes[x, y - 1, z].GetComponent<Cube>().above.SetActive(false);
+            }
+
+            if (z < depth - 1 && cubes[x, y, z + 1] != null)
+            {
+                cubes[x, y, z + 1].GetComponent<Cube>().back.SetActive(false);
+            }
+
+            if (z > 0 && cubes[x, y, z - 1] != null)
+            {
+                cubes[x, y, z - 1].GetComponent<Cube>().front.SetActive(false);
+            }
+
+            if (x < width - 1 && cubes[x + 1, y, z] != null)
+            {
+                cubes[x + 1, y, z].GetComponent<Cube>().left.SetActive(false);
+            }
+
+            if (x > 0 && cubes[x - 1, y, z] != null)
+            {
+                cubes[x - 1, y, z].GetComponent<Cube>().right.SetActive(false);
+            }
+        }
+    }
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
